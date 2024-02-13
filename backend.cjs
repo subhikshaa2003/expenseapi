f8aeba3b9dd511f3b2db6450c19342adb2058b2f
subhikshaa2003 <subhikshaa2003@gmail.com>
2024-02-13 02:37:43 -0800

    updated

--- backend.cjs
+++ backend.cjs
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors =require('cors')
 const bodyParser= require('body-parser')
 // Importing required functions from dbConnection.cjs
 const {connectToDb, getDb} = require('./dbconnect.cjs');
